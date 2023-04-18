78b4aeac09bf67cb9f887061c9725829fd4b0621
mdue1337 <martin@familiendue.dk>
2023-04-18 13:38:15 +0200

    setting js

--- setting.js
+++ setting.js
@@ -1,12 +1,13 @@
-let user = localStorage.getItem("UserName");
+let user = JSON.parse(localStorage.getItem("UserName"));
+
 document.getElementById("BrugerNavnInput").value = user;
 
-let email = localStorage.getItem("Email");
+let email = JSON.parse(localStorage.getItem("Email"));
 document.getElementById("EmailInput").value = email;
 
-let password = localStorage.getItem("password");
+let password = JSON.parse(localStorage.getItem("password"));
 document.getElementById("PasswordInput").value = password;
 
 function bekræftÆndring(){
-
+    
 }
