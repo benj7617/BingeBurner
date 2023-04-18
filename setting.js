d988a849025d4eb8636023ac1b39aeb8116bf8df
The-cool-guy <71141167+The-cool-guy@users.noreply.github.com>
2023-04-18 13:04:58 +0200

    Lavet alt på setting undtagen bekræft knappen

---
+++ setting.js
@@ -0,0 +1,14 @@
+
+
+let user = localStorage.getItem("username");
+document.getElementById("BrugerNavnInput").value = user;
+
+let email = localStorage.getItem("username");
+document.getElementById("EmailInput").value = email;
+
+let password = localStorage.getItem("username");
+document.getElementById("PasswordInput").value = password;
+
+function bekræftÆndring(){
+
+}
