0229ebe329cff16a14c8f8ee1ba305cb6de1e2b3
mdue1337 <martin@familiendue.dk>
2023-04-18 13:15:08 +0200

    motion script

--- motion.js
+++ motion.js
@@ -1,3 +1,45 @@
-export let getPoints = (function () {
+let getPoints = (function () {
     document.getElementById("userPoints").innerHTML = localStorage.getItem("points") + " points";
 })();
+
+let pointModel = (distance, time, k) => {
+    let sum = ((distance * 1000)/time * k);
+    return sum;
+}
+
+let addPoints = (sum) =>{
+    if(confirm("You will recieve " + sum + " points! Are you finished with your workout?") == true){
+        alert(sum + " points added to your acount");
+        points = parseInt(localStorage.getItem("points"));
+        point = points + parseInt(sum);
+        localStorage.setItem("points", JSON.stringify(point));
+        location.reload();
+    }
+}
+
+function CalcPointsWalk(){
+    let distance = document.getElementById("walk-km").value;
+    let time = document.getElementById("walk-tid").value;
+    let k = 1;
+
+    let sum = pointModel(distance, time, k);
+    addPoints(sum);
+}
+
+function CalcPointsRun(){
+    let distance = document.getElementById("run-km").value;
+    let time = document.getElementById("run-tid").value;
+    let k = 2;
+
+    let sum = pointModel(distance, time, k);
+    addPoints(sum);
+}
+
+function CalcPointsCycle(){
+    let distance = document.getElementById("cykel-km").value;
+    let time = document.getElementById("cykel-tid").value;
+    let k = 1.5;
+
+    let sum = pointModel(distance, time, k);
+    addPoints(sum);
+}
