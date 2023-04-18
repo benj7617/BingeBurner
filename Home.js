d51f4ce9e69f31505938536fd8f5c3237b32aebe
Aayan0909 <Aayanminhas0909@gmail.com>
2023-04-18 13:57:45 +0200

    username uden ""

--- Home.js
+++ Home.js
@@ -2,3 +2,7 @@ let getPoints = (function () {
     document.getElementById("points").innerHTML = localStorage.getItem("points") + " points";
 })();
 
+let getUserName = (function () {
+    document.getElementById("userName").innerHTML = JSON.parse(localStorage.getItem("UserName"));
+})();
+
