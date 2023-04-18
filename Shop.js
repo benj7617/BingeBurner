aff99c53553822ab46002ffcd68ac48d6c27b529
benj7617 <116954841+benj7617@users.noreply.github.com>
2023-04-18 14:30:20 +0200

    nu kan man købe ting
    wowowowowowo

--- Shop.js
+++ Shop.js
@@ -1,3 +1,13 @@
 let getPoints = (function () {
     document.getElementById("userPoints").innerHTML = localStorage.getItem("points") + " points";
 })();
+
+let addPoints = (sum) =>{
+    if(confirm("Du vil betale " + sum + " point! Er du sikker?") == true){
+        alert(sum + " point fjernet fra din konto! Du har nu adgang til den valgte undeerholdning");
+        points = parseInt(localStorage.getItem("points"));
+        point = points + parseInt(sum);
+        localStorage.setItem("points", JSON.stringify(point));
+        location.reload();
+    }
+}
