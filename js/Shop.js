806ec8c98fed8ad183380ee723c0c9229870c67f
mdue1337 <martin@familiendue.dk>
2023-04-19 08:54:10 +0200

    De er connected nu

--- js/Shop.js
+++ js/Shop.js
@@ -2,6 +2,14 @@ let getPoints = (function () {
     document.getElementById("userPoints").innerHTML = localStorage.getItem("points") + " points";
 })();
 
+function sendToNetflix(){
+    let NetflixWindow = window.open("https://www.netflix.com/", "_blank");
+    setTimeout(function() {
+        NetflixWindow.close()
+        alert("Page will close in 5 sec")
+    }, 5000);
+}
+
 let checkPoints = (price) => {
     if(confirm("Do you want to buy this option?") == true){
         let balance = JSON.parse(localStorage.getItem("points"));
@@ -10,7 +18,7 @@ let checkPoints = (price) => {
             localStorage.setItem("points", balance);
             alert("30 min of Netflix bought for " + price);
             location.reload();
-            // nu skal du videresendes - er det muligt at lave en ny fane hvor netflix.com åbner?
+            sendToNetflix();
         }
         else{
             alert("not suffient balance")
